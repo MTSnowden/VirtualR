0e667a51814d6feb94776f1462fa202bb9ba20f9
mtsnowden <mtsnowden@gmail.com>
2025-01-05 10:02:11 -0600

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { Workflow } from 'lucide-react'
 import FeatureSection from './components/FeatureSection'
+import Footer from './components/Footer'
 import HeroSection from './components/HeroSection'
 import Navbar from './components/Navbar'
-import React from 'react'
+import Pricing from './components/Pricing'
+import Testimonials from './components/Testimonials'
+import Workflow from './components/Workflow'
 
 const App = () => {
   return (
@@ -11,7 +13,10 @@ const App = () => {
     <div className="max-w-7xl mx-auto pt-20 px-6">
     <HeroSection />
     <FeatureSection />
-    <Workflow
+    <Workflow />
+    <Pricing />
+    <Testimonials />
+    <Footer />
     </div>
 
     
